159e8bc41ad95a54a134ef4c36bc401ce9e8b4f5
moonlight <32425955+Zacharia2@users.noreply.github.com>
2024-04-22 15:03:48 +0800

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -7,8 +7,8 @@ export const sharedPageComponents: SharedLayout = {
   header: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/Zacharia2/T2NLC-PubWiki",
+      LICENSE: "https://creativecommons.org/licenses/by-nc-sa/4.0/"
     },
   }),
 }
